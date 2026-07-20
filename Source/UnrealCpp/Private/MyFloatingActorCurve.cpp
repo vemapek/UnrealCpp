75299cc3a83dc1449844fa9a0bb574937738ef3c
user1 <user1@USER>
2026-07-20 16:06:56 +0900

    0720 언리얼 C++ 1일차

---
+++ Source/UnrealCpp/Private/MyFloatingActorCurve.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyFloatingActorCurve.h"
+
+void AMyFloatingActorCurve::TickTock(float DeltaTime)
+{
+	float CurveValue = Curve->GetFloatValue(RunningTime);
+	MainMesh->SetRelativeLocation(FVector(0.0f, 0.0f, CurveValue * Amplitude));
+
+	RunningTime += DeltaTime;
+	if (RunningTime > 2.0f)
+	{
+		RunningTime = 0.0f;
+	}
+}
