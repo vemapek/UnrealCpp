e2cb86cbde08cd780659c23b1cc4fe46340b0cd7
vemapek <dmb0501@naver.com>
2026-07-29 17:31:36 +0900

    모듈 정의 파일 복구

---
+++ Source/UnrealCpp/UnrealCpp.h
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#define ECC_Weapon ECC_GameTraceChannel1
+#define ECC_Player ECC_GameTraceChannel2
+#define ECC_Enemy ECC_GameTraceChannel3
+
